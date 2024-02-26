646a4b9e811dd6111866415c317b4e1ac39c77a2
Tim Ogden <togden1999@gmail.com>
2024-02-26 13:01:07 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          <a href='intervalIdentification'>Interval Identification</a>
+        </li>
+        <li>
+          <a href='chordIdentification'>Chord Identification</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
