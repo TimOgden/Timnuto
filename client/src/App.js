0b54b313803b1a99bc04d0f7fd6c45914a844fe5
Tim <togden1999@gmail.com>
2024-03-04 20:54:59 -0500

    Add simple google log in button

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 import HomePage from './HomePage';
 import IntervalTraining from './IntervalTraining';
 import ChordTraining from './ChordTraining';
 import './App.css';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/intervalTraining" element={<IntervalTraining />} />
-        <Route path="/chordTraining" element={<ChordTraining />} />
-      </Routes>
-    </Router>
+    <>
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage}/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/intervalTraining" element={<IntervalTraining />} />
+          <Route path="/chordTraining" element={<ChordTraining />} />
+        </Routes>
+      </Router>
+    </>
+    
   );
 }
 
